51fda3c174dce57521d602d3a7cab72c1659d771
anhcoder03 <nguyenphianh161003@gmail.com>
2023-05-27 21:41:34 +0700

    base project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { ClientLayoutComponent } from './layouts/client-layout/client-layout.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { DashboardHeaderComponent } from './components/dashboard/dashboard-header/dashboard-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdminLayoutComponent,
+    ClientLayoutComponent,
+    HomepageComponent,
+    DashboardHeaderComponent
   ],
   imports: [
     BrowserModule,
